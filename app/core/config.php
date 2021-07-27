0e6f3da251b1615c52941f870859ca275b926d88
Luis Gustavo <lluisgusttavo@gmail.com>
2021-07-27 20:36:22 -0300

    Testing gitignore

--- app/core/config.php
+++ app/core/config.php
@@ -5,7 +5,7 @@ define('WEBSITE_TITLE', 'techITems');
 //DATABASE INFO
 define('DB_NAME', 'luisde64_techitems');
 define('DB_USER', 'luisde64_techADM');
-define('DB_PASS', '5GfB{E8{#-eQ');
+define('DB_PASS', '%qG$h]RUMh[g');
 define('DB_HOST', 'br994.hostgator.com.br');
 define('DB_TYPE', 'mysql');
 define('THEME', 'main/');
